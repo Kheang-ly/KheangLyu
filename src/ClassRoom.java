dfb5e8e04aab1bdfc8e69be9cd4032a2fb72e0c9
longpiseth2002 <stevkhat121212@gmail.com>
2024-02-11 00:39:34 +0700

    Update

--- src/ClassRoom.java
+++ src/ClassRoom.java
@@ -1,2 +1,72 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class ClassRoom {
+
+    private int classNo;
+    private String className;
+    private static ArrayList<ClassRoom> classRooms = new ArrayList<>();
+    private Shift shifts = new Shift();
+
+    public ClassRoom() {
+    }
+
+    public ClassRoom(int classNo, String className ) {
+        this.classNo = classNo;
+        this.className = className;
+        classRooms.add(this);
+    }
+
+    public int getClassNo() {
+        return classNo;
+    }
+
+    public void setClassNo(int classNo) {
+        this.classNo = classNo;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public static ArrayList<ClassRoom> getClassRooms() {
+        return classRooms;
+    }
+
+    public static void setClassRooms(ArrayList<ClassRoom> classRooms) {
+        ClassRoom.classRooms = classRooms;
+    }
+
+    public Shift getShifts() {
+        return shifts;
+    }
+
+    public void setShifts(Shift shifts) {
+        this.shifts = shifts;
+    }
+
+    public static boolean isAvailableClassRoom (ClassRoom classRoom , int id ){
+        if(Objects.equals(classRoom.classNo,id)){
+            return true;
+        }
+        return false;
+    }
+
+
+    public static void checkClassRoom ( int id ){
+        for( int i=0 ; i<classRooms.size() ; i++ ){
+            if(isAvailableClassRoom(classRooms.get(i),id)){
+                System.out.println(" Class Name : ".concat(classRooms.get(i).getClassName()));
+                System.out.println( new ClassRoom().getShifts().schedule());
+                return;
+            }
+        }
+        System.out.println(" Class number not found ");
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,13 @@ public class Main {
         Teacher teacher = new Teacher(1,"Sza","male");
         Teacher teacher1 = new Teacher(2,"Justint","male");
         Teacher teacher2 = new Teacher(3,"Weeken","male");
-        Schedule schedule=new Schedule();
-        Schedule.show(schedule);
+        ClassRoom classRoom = new ClassRoom();
+
+        Shift shift = new Shift();
+        ClassRoom classRoom1 = new ClassRoom(101,"M1");
+        ClassRoom classRoom2 = new ClassRoom(102,"M2");
+        ClassRoom.checkClassRoom(102);
+
 
 
     }
